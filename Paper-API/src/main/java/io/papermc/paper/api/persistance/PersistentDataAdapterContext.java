500ccc57e6dca018c99d24601d5759c43bce9d9c
Sam Turner <samt@thoriumcube.org>
2023-09-07 13:06:03 +0100

    Add persistence

---
+++ Paper-API/src/main/java/io/papermc/paper/api/persistance/PersistentDataAdapterContext.java
@@ -0,0 +1,17 @@
+package io.papermc.paper.api.persistance;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This interface represents the context in which the {@link PersistentDataType} can
+ * serialize and deserialize the passed values.
+ */
+public interface PersistentDataAdapterContext {
+    /**
+     * Creates a new and empty meta container instance.
+     *
+     * @return the fresh container instance
+     */
+    @NotNull
+    PersistentDataContainer newPersistentDataContainer();
+}
