4dce0090000ba0be630428db3fa2ceddc808a989
Sam Turner <samt@thoriumcube.org>
2023-09-07 12:05:24 +0100

    Add attributes

---
+++ Paper-API/src/main/java/io/papermc/paper/api/attribute/Attributable.java
@@ -0,0 +1,29 @@
+package io.papermc.paper.api.attribute;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Represents an object which may contain attributes.
+ */
+public interface Attributable {
+    /**
+     * Gets the specified attribute instance from the object. This instance will
+     * be backed directly to the object and any changes will be visible at once.
+     *
+     * @param attribute the attribute to get
+     * @return the attribute instance or null if not applicable to this object
+     */
+    @Nullable
+    AttributeInstance getAttribute(@NotNull Attribute attribute);
+
+    // Paper start
+    /**
+     * Registers a generic attribute to that attributable instance.
+     * Allows it to add attributes not registered by default to that entity.
+     *
+     * @param attribute the generic attribute to register
+     */
+    void registerAttribute(@NotNull Attribute attribute);
+    // Paper end
+}
---
+++ Paper-API/src/main/java/io/papermc/paper/api/attribute/AttributeInstance.java
@@ -0,0 +1,81 @@
+package io.papermc.paper.api.attribute;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+/**
+ * Represents a mutable instance of an attribute and its associated modifiers
+ * and values.
+ */
+public interface AttributeInstance {
+
+    /**
+     * The attribute pertaining to this instance.
+     *
+     * @return the attribute
+     */
+    @NotNull
+    Attribute getAttribute();
+
+    /**
+     * Base value of this instance before modifiers are applied.
+     *
+     * @return base value
+     */
+    double getBaseValue();
+
+    /**
+     * Set the base value of this instance.
+     *
+     * @param value new base value
+     */
+    void setBaseValue(double value);
+
+    /**
+     * Get all modifiers present on this instance.
+     *
+     * @return a copied collection of all modifiers
+     */
+    @NotNull
+    Collection<AttributeModifier> getModifiers();
+
+    /**
+     * Add a modifier to this instance.
+     *
+     * @param modifier to add
+     */
+    void addModifier(@NotNull AttributeModifier modifier);
+
+    // Paper start - Transient modifier API
+    /**
+     * Add a transient modifier to this instance.
+     * Transient modifiers are not persisted (saved with the NBT data)
+     *
+     * @param modifier to add
+     */
+    void addTransientModifier(@NotNull AttributeModifier modifier);
+    // Paper end
+
+    /**
+     * Remove a modifier from this instance.
+     *
+     * @param modifier to remove
+     */
+    void removeModifier(@NotNull AttributeModifier modifier);
+
+    /**
+     * Get the value of this instance after all associated modifiers have been
+     * applied.
+     *
+     * @return the total attribute value
+     */
+    double getValue();
+
+    /**
+     * Gets the default value of the Attribute attached to this instance.
+     *
+     * @return server default value
+     */
+    double getDefaultValue();
+}
