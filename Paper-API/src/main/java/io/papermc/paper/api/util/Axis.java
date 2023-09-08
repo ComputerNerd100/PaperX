77f1dc0a61151e021ed45ba7d311e021fdcbe35c
Sam Turner <samt@thoriumcube.org>
2023-09-08 10:26:09 +0100

    Add Axis to util

---
+++ Paper-API/src/main/java/io/papermc/paper/api/util/Axis.java
@@ -0,0 +1,17 @@
+package io.papermc.paper.api.util;
+
+public enum Axis {
+    /**
+     * The x axis.
+     */
+    X,
+    /**
+     * The y axis.
+     */
+    Y,
+    /**
+     * The z axis.
+     */
+    Z;
+}
+
