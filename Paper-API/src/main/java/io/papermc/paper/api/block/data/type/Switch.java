4c2b9f2c147d4fd7ea813223769819cddc7fc2d2
Sam Turner <samt@thoriumcube.org>
2023-09-11 14:28:09 +0100

    Remove deprecated methods and enum

--- Paper-API/src/main/java/io/papermc/paper/api/block/data/type/Switch.java
+++ Paper-API/src/main/java/io/papermc/paper/api/block/data/type/Switch.java
@@ -6,45 +6,5 @@ import io.papermc.paper.api.block.data.Powerable;
 import org.jetbrains.annotations.NotNull;
 
 public interface Switch extends Directional, FaceAttachable, Powerable {
-
-    /**
-     * Gets the value of the 'face' property.
-     *
-     * @return the 'face' value
-     * @deprecated use {@link #getAttachedFace()}
-     */
-    @NotNull
-    @Deprecated
-    Face getFace();
-
-    /**
-     * Sets the value of the 'face' property.
-     *
-     * @param face the new 'face' value
-     * @deprecated use {@link #setAttachedFace(AttachedFace)}
-     */
-    @Deprecated
-    void setFace(@NotNull Face face);
-
-    /**
-     * The face to which a switch type block is stuck.
-     *
-     * @deprecated use {@link AttachedFace}
-     */
-    @Deprecated
-    enum Face {
-        /**
-         * The switch is mounted to the floor and pointing upwards.
-         */
-        FLOOR,
-        /**
-         * The switch is mounted to the wall.
-         */
-        WALL,
-        /**
-         * The switch is mounted to the ceiling and pointing downwards.
-         */
-        CEILING;
-    }
 }
 
