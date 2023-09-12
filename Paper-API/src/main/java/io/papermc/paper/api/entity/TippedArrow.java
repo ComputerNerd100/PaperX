62764a2a4231507cff4e08ed706c2700e195d6d8
Sam Turner <samt@thoriumcube.org>
2023-09-12 12:13:34 +0100

    Update javadoc

--- Paper-API/src/main/java/io/papermc/paper/api/entity/TippedArrow.java
+++ Paper-API/src/main/java/io/papermc/paper/api/entity/TippedArrow.java
@@ -3,6 +3,8 @@ package io.papermc.paper.api.entity;
 /**
  * @deprecated tipped status depends only on base potion type not being
  * UNCRAFTABLE and effects being empty.
+ *
+ * TODO: Confirm with paper
  */
 @Deprecated
 public interface TippedArrow extends Arrow { }
