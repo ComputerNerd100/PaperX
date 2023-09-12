fdecf09068a8742c2523d7d7752da4227b5d155e
Sam Turner <samt@thoriumcube.org>
2023-09-12 10:47:54 +0100

    Add other kyori deps

--- Paper-API/build.gradle.kts
+++ Paper-API/build.gradle.kts
@@ -1,6 +1,14 @@
 dependencies {
-    implementation("net.kyori:adventure-api:4.13.0")
+    api("net.kyori:adventure-api:4.14.0")
+    api("net.kyori:adventure-text-minimessage:4.14.0")
+    api("net.kyori:adventure-text-serializer-ansi:4.14.0")
+    api("net.kyori:adventure-text-serializer-gson:4.14.0")
+    api("net.kyori:adventure-text-serializer-json:4.14.0")
+    api("net.kyori:adventure-text-serializer-legacy:4.14.0")
+    api("net.kyori:adventure-text-serializer-plain:4.14.0")
+
     api("com.google.guava:guava:31.1-jre")
+
 }
 
 description = "Paper-Api"
