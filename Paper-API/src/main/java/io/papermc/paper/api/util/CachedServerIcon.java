736edaadfe84146770b0953e0c51fc6166242d0f
Sam Turner <samt@thoriumcube.org>
2023-09-14 13:13:36 +0100

    Add CachedServerIcon in util

---
+++ Paper-API/src/main/java/io/papermc/paper/api/util/CachedServerIcon.java
@@ -0,0 +1,22 @@
+package io.papermc.paper.api.util;
+
+import io.papermc.paper.api.Server;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * This is a cached version of a server-icon. Its internal representation
+ * and implementation is undefined.
+ *
+ * @see Server#getServerIcon()
+ * @see Server#loadServerIcon(java.awt.image.BufferedImage)
+ * @see Server#loadServerIcon(java.io.File)
+ * @see ServerListPingEvent#setServerIcon(CachedServerIcon)
+ */
+public interface CachedServerIcon {
+    @Nullable
+    public String getData();
+    default boolean isEmpty() {
+        return getData() == null;
+    }
+}
+
