195a86bb08cd99f597aec34ebb613504fb8d1355
Sam Turner <samt@thoriumcube.org>
2023-09-14 13:20:36 +0100

    Add Vibration in util

---
+++ Paper-API/src/main/java/io/papermc/paper/api/util/Vibration.java
@@ -0,0 +1,82 @@
+package io.papermc.paper.api.util;
+
+import io.papermc.paper.api.block.Block;
+import io.papermc.paper.api.entity.Entity;
+import io.papermc.paper.api.location.Location;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a vibration from a Skulk sensor.
+ */
+public class Vibration {
+
+    private final Location origin;
+    private final Destination destination;
+    private final int arrivalTime;
+
+    public Vibration(@NotNull Destination destination, @NotNull int arrivalTime) {
+        this.destination = destination;
+        this.arrivalTime = arrivalTime;
+        this.origin = new Location(null, 0, 0, 0); // Dummy origin because getter expects null
+    }
+
+    /**
+     * Get the vibration destination.
+     *
+     * @return destination
+     */
+    @NotNull
+    public Destination getDestination() {
+        return destination;
+    }
+
+    /**
+     * Get the vibration arrival time in ticks.
+     *
+     * @return arrival time
+     */
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public interface Destination {
+
+        public static class EntityDestination implements Destination {
+
+            private final Entity entity;
+
+            public EntityDestination(@NotNull Entity entity) {
+                this.entity = entity;
+            }
+
+            @NotNull
+            public Entity getEntity() {
+                return entity;
+            }
+        }
+
+        public static class BlockDestination implements Destination {
+
+            private final Location block;
+
+            public BlockDestination(@NotNull Location block) {
+                this.block = block;
+            }
+
+            public BlockDestination(@NotNull Block block) {
+                this(block.getLocation());
+            }
+
+            @NotNull
+            public Location getLocation() {
+                return block;
+            }
+
+            @NotNull
+            public Block getBlock() {
+                return block.getBlock();
+            }
+        }
+    }
+}
+
