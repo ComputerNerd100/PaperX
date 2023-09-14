1e743bd7364e291b9e92756af3a89807735aac57
Sam Turner <samt@thoriumcube.org>
2023-09-14 12:57:41 +0100

    Add permission in util

---
+++ Paper-API/src/main/java/io/papermc/paper/api/util/permission/BroadcastPermissions.java
@@ -0,0 +1,25 @@
+package io.papermc.paper.api.util.permission;
+
+import io.papermc.paper.api.permisson.Permission;
+import io.papermc.paper.api.permisson.PermissionDefault;
+import org.jetbrains.annotations.NotNull;
+
+public final class BroadcastPermissions {
+    private static final String ROOT = "bukkit.broadcast";
+    private static final String PREFIX = ROOT + ".";
+
+    private BroadcastPermissions() {}
+
+    @NotNull
+    public static Permission registerPermissions(@NotNull Permission parent) {
+        Permission broadcasts = DefaultPermissions.registerPermission(ROOT, "Allows the user to receive all broadcast messages", parent);
+
+        DefaultPermissions.registerPermission(PREFIX + "admin", "Allows the user to receive administrative broadcasts", PermissionDefault.OP, broadcasts);
+        DefaultPermissions.registerPermission(PREFIX + "user", "Allows the user to receive user broadcasts", PermissionDefault.TRUE, broadcasts);
+
+        broadcasts.recalculatePermissibles();
+
+        return broadcasts;
+    }
+}
+
