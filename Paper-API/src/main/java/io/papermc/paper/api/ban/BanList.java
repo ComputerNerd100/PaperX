4893ddae2efd117aa0581c4a2ad450ee4a20780d
Sam Turner <samt@thoriumcube.org>
2023-09-14 13:43:25 +0100

    Remove deprecated methods

--- Paper-API/src/main/java/io/papermc/paper/api/ban/BanList.java
+++ Paper-API/src/main/java/io/papermc/paper/api/ban/BanList.java
@@ -39,46 +39,12 @@ public interface BanList<T> {
 
     /**
      * Gets a {@link BanEntry} by target.
-     * <p>
-     * Bans by name for ban type {@link Type#NAME NAME} are no longer supported and this method will return
-     * null when trying to request them. The replacement is bans by UUID.
-     *
-     * @param target entry parameter to search for
-     * @return the corresponding entry, or null if none found
-     * @deprecated see {@link #getBanEntry(Object)}
-     */
-    @Deprecated
-    @Nullable
-    public <E extends BanEntry<? super T>> E getBanEntry(@NotNull String target); // Paper
-
-    /**
-     * Gets a {@link BanEntry} by target.
      *
      * @param target entry parameter to search for
      * @return the corresponding entry, or null if none found
      */
     @Nullable
     public BanEntry<T> getBanEntry(@NotNull T target);
-
-    /**
-     * Adds a ban to this list. If a previous ban exists, this will
-     * update the previous entry.
-     * <p>
-     * Bans by name for ban type {@link Type#NAME NAME} are no longer supported and this method will return
-     * null when trying to request them. The replacement is bans by UUID.
-     *
-     * @param target the target of the ban
-     * @param reason reason for the ban, null indicates implementation default
-     * @param expires date for the ban's expiration (unban), or null to imply
-     *     forever
-     * @param source source of the ban, null indicates implementation default
-     * @return the entry for the newly created ban, or the entry for the
-     *     (updated) previous ban
-     * @deprecated see {@link #addBan(Object, String, Date, String)}
-     */
-    @Deprecated
-    @Nullable
-    public <E extends BanEntry<? super T>> E addBan(@NotNull String target, @Nullable String reason, @Nullable Date expires, @Nullable String source); // Paper
 
     /**
      * Adds a ban to this list. If a previous ban exists, this will
@@ -129,16 +95,6 @@ public interface BanList<T> {
      * Gets a set containing every {@link BanEntry} in this list.
      *
      * @return an immutable set containing every entry tracked by this list
-     * @deprecated This return a generic class, prefer use {@link #getEntries()}
-     */
-    @Deprecated
-    @NotNull
-    public Set<BanEntry> getBanEntries();
-
-    /**
-     * Gets a set containing every {@link BanEntry} in this list.
-     *
-     * @return an immutable set containing every entry tracked by this list
      */
     @NotNull
     public <E extends BanEntry<? super T>> Set<E> getEntries(); // Paper
@@ -157,18 +113,6 @@ public interface BanList<T> {
     public boolean isBanned(@NotNull T target);
 
     /**
-     * Gets if a {@link BanEntry} exists for the target, indicating an active
-     * ban status.
-     *
-     * @param target the target to find
-     * @return true if a {@link BanEntry} exists for the target, indicating an
-     *     active ban status, false otherwise
-     * @deprecated see {@link #isBanned(Object)}
-     */
-    @Deprecated
-    public boolean isBanned(@NotNull String target);
-
-    /**
      * Removes the specified target from this list, therefore indicating a
      * "not banned" status.
      * <p>
@@ -179,14 +123,4 @@ public interface BanList<T> {
      */
     public void pardon(@NotNull T target);
 
-    /**
-     * Removes the specified target from this list, therefore indicating a
-     * "not banned" status.
-     *
-     * @param target the target to remove from this list
-     *
-     * @deprecated see {@link #pardon(Object)}
-     */
-    @Deprecated
-    public void pardon(@NotNull String target);
 }
