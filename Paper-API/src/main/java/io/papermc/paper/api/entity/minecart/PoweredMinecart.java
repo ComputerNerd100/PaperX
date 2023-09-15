be4e854b94556a853ac3f67ea52aa213fe684426
Sam Turner <samt@thoriumcube.org>
2023-09-15 11:09:42 +0100

    Cleanup code

--- Paper-API/src/main/java/io/papermc/paper/api/entity/minecart/PoweredMinecart.java
+++ Paper-API/src/main/java/io/papermc/paper/api/entity/minecart/PoweredMinecart.java
@@ -14,41 +14,41 @@ public interface PoweredMinecart extends Minecart {
      *
      * @return Number of ticks until the minecart runs out of fuel
      */
-    public int getFuel();
+    int getFuel();
 
     /**
      * Set the number of ticks until the minecart runs out of fuel.
      *
      * @param fuel Number of ticks until the minecart runs out of fuel
      */
-    public void setFuel(int fuel);
+    void setFuel(int fuel);
 
     /**
      * Get the x push of the minecart.
      *
      * @return The x push of the minecart
      */
-    public double getPushX();
+    double getPushX();
 
     /**
      * Get the z push of the minecart.
      *
      * @return The z push of the minecart
      */
-    public double getPushZ();
+    double getPushZ();
 
     /**
      * Set the x push of the minecart.
      *
      * @param xPush The new x push of the minecart
      */
-    public void setPushX(double xPush);
+    void setPushX(double xPush);
 
     /**
      * Set the z push of the minecart.
      *
      * @param zPush The new z push of the minecart
      */
-    public void setPushZ(double zPush);
+    void setPushZ(double zPush);
 }
 
