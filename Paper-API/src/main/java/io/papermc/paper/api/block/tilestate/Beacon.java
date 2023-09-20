5245ded528e8bae2f4afb7d62a15c053cf4c4665
Sam Turner <samt@thoriumcube.org>
2023-09-20 15:05:31 +0100

    Add Beacon in block

---
+++ Paper-API/src/main/java/io/papermc/paper/api/block/tilestate/Beacon.java
@@ -0,0 +1,86 @@
+package io.papermc.paper.api.block.tilestate;
+
+import io.papermc.paper.api.entity.LivingEntity;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Collection;
+
+/**
+ * Represents a captured state of a beacon.
+ */
+public interface Beacon extends LockableTileState {
+
+    /**
+     * Returns the list of players within the beacon's range of effect.
+     * <p>
+     * This will return an empty list if the block represented by this state is
+     * no longer a beacon.
+     *
+     * @return the players in range
+     * @throws IllegalStateException if this block state is not placed
+     */
+    @NonNull
+    Collection<LivingEntity> getEntitiesInRange();
+
+    /**
+     * Returns the tier of the beacon pyramid (0-4). The tier refers to the
+     * beacon's power level, based on how many layers of blocks are in the
+     * pyramid. Tier 1 refers to a beacon with one layer of 9 blocks under it.
+     *
+     * @return the beacon tier
+     */
+    int getTier();
+
+    /**
+     * Returns the primary effect set on the beacon
+     *
+     * @return the primary effect or null if not set
+     */
+    @Nullable
+    PotionEffect getPrimaryEffect();
+
+    /**
+     * Set the primary effect on this beacon, or null to clear.
+     *
+     * @param effect new primary effect
+     */
+    void setPrimaryEffect(@Nullable PotionEffectType effect);
+
+    /**
+     * Returns the secondary effect set on the beacon.
+     *
+     * @return the secondary effect or null if no secondary effect
+     */
+    @Nullable
+    PotionEffect getSecondaryEffect();
+
+    /**
+     * Set the secondary effect on this beacon, or null to clear. Note that tier
+     * must be &gt;= 4 for this effect to be active.
+     *
+     * @param effect desired secondary effect
+     */
+    void setSecondaryEffect(@Nullable PotionEffectType effect);
+
+    /**
+     * Gets the effect range of this beacon.
+     * A negative range value means the beacon is using its default range based on tier.
+     * @return Either the custom range set with {@link #setEffectRange(double)} or the range based on the beacon tier.
+     */
+    double getEffectRange();
+
+    /**
+     * Sets the effect range of the beacon
+     * A negative range value means the beacon is using its default range based on tier.
+     * @param range Radius of effect range.
+     */
+    void setEffectRange(double range);
+
+    /**
+     * Resets the custom range from this beacon and falls back to the range based on the beacon tier.
+     * Shortcut for setting the effect range to a negative number.
+     */
+    void resetEffectRange();
+}
+
