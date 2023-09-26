dcdde17f8e8cc211419b3d2119521f7f087aa4ac
Sam Turner <samt@thoriumcube.org>
2023-09-26 14:10:41 +0100

    Make adventure version a field

--- Paper-API/build.gradle.kts
+++ Paper-API/build.gradle.kts
@@ -1,11 +1,12 @@
 dependencies {
-    api("net.kyori:adventure-api:4.14.0")
-    api("net.kyori:adventure-text-minimessage:4.14.0")
-    api("net.kyori:adventure-text-serializer-ansi:4.14.0")
-    api("net.kyori:adventure-text-serializer-gson:4.14.0")
-    api("net.kyori:adventure-text-serializer-json:4.14.0")
-    api("net.kyori:adventure-text-serializer-legacy:4.14.0")
-    api("net.kyori:adventure-text-serializer-plain:4.14.0")
+    val adventureVersion = "4.14.0"
+    api("net.kyori:adventure-api:$adventureVersion")
+    api("net.kyori:adventure-text-minimessage:$adventureVersion")
+    api("net.kyori:adventure-text-serializer-ansi:$adventureVersion")
+    api("net.kyori:adventure-text-serializer-gson:$adventureVersion")
+    api("net.kyori:adventure-text-serializer-json:$adventureVersion")
+    api("net.kyori:adventure-text-serializer-legacy:$adventureVersion")
+    api("net.kyori:adventure-text-serializer-plain:$adventureVersion")
 
     api("com.google.guava:guava:32.1.2-jre")
     implementation("org.apache.commons:commons-lang3:3.13.0")
