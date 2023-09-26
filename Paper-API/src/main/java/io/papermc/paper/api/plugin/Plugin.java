08c743c92529dd65646bfcc0c974e7524f574a3c
Sam Turner <samt@thoriumcube.org>
2023-09-26 14:53:10 +0100

    Add methods to Plugin

--- Paper-API/src/main/java/io/papermc/paper/api/plugin/Plugin.java
+++ Paper-API/src/main/java/io/papermc/paper/api/plugin/Plugin.java
@@ -1,7 +1,23 @@
 package io.papermc.paper.api.plugin;
 
+import io.papermc.paper.api.Server;
+import io.papermc.paper.api.world.World;
+import io.papermc.paper.api.world.generator.BiomeProvider;
+import io.papermc.paper.api.world.generator.ChunkGenerator;
+
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
 public interface Plugin {
+    String name();
     void onLoad();
     void onEnable();
     void onDisable();
+    Server server();
+    Path dataFolder();
+    Logger logger();
+    boolean enabled();
+    String description();
+    ChunkGenerator defaultWorldGenerator(World world, String id);
+    BiomeProvider defaultBiomeProvider(World world, String id);
 }
